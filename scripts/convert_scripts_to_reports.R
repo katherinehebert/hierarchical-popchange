f81d6a9a29c138be96fbaec7c58055e2607dc1a2
Katherine Hébert <katherine.ahebert@gmail.com>
2023-01-26 15:25:03 -0500

    start on working through the HGAM

--- scripts/convert_scripts_to_reports.R
+++ scripts/convert_scripts_to_reports.R
@@ -3,4 +3,6 @@
 # set working directory to folder where I want the reports
 setwd("scripts/")
 
-knitr::spin("hmsc_fish.R", format = "Rmd", knit = FALSE)
+# knitr::spin("hmsc_fish.R", format = "Rmd", knit = FALSE)
+
+knitr::spin("hgam_fish.R", format = "Rmd", knit = FALSE)
