33e5c26c4582b276dcf3bf1302a55771dbf9b47b
Katherine Hébert <katherine.ahebert@gmail.com>
2024-12-16 18:53:29 +0100

    new latent figs

--- scripts/07-plot-latentfactors.R
+++ scripts/07-plot-latentfactors.R
@@ -1,12 +1,19 @@
 # Inspect the latent factors
 
 library(mvgam)
+library(rstan)
+library(patchwork)
+library(tidyverse)
 
-biomass_scaling = readRDS("outputs/biomass_scaling.rds")
+biomass = readRDS("outputs/biomass_scaling.rds")
+biomasses = as.data.frame(biomass$YMeans)
+colnames(biomasses) = "biomass_mean"
+
 derivs_pops_df = readRDS("outputs/derivs_pops_df.rds") |> 
-  dplyr::filter(year > 1998) |>
+  #dplyr::filter(year > 1998) |>
   dplyr::group_by(name) |>
-  dplyr::summarise(mean_rate = mean(value))
+  dplyr::summarise(mean_rate = mean(value),
+                   sd_rate = sd(value))
 
 mod1 = readRDS(paste0("outputs/gam_hierarchical_gp.rds"))
 
@@ -34,6 +41,9 @@ loadings_plot =
   ) 
 loadings_plot = dplyr::full_join(loadings_plot, derivs_pops_df)
 
+biomasses$name <- loadings_plot$name
+loadings_plot = dplyr::full_join(loadings_plot, biomasses)
+
 library(ggplot2)
 ggplot(data = loadings_plot) +
   geom_hline(yintercept = 0) +
@@ -47,31 +57,69 @@ ggplot(data = loadings_plot) +
 ggplot(data = loadings_plot) +
   geom_hline(yintercept = 0) +
   geom_vline(xintercept = 0) +
-  geom_point(aes(x = LV1, y = LV2, fill = mean_rate), pch = 21, size = 3) +
-  theme_minimal()  +
-  scale_fill_distiller(palette = "Spectral", direction = 1)
+  geom_point(aes(x = LV1, y = LV2, fill = mean_rate), pch = 21, size = 4) +
+  ggpubr::theme_pubr()  +
+  scale_fill_distiller(palette = "Spectral", direction = 1, limits = c(-0.09, 0.09)) +
+  labs(fill = "Average\ngrowth rate", x = "Latent variable 1", y = "Latent variable 2") +
+  theme(legend.position  = "right") +
+  coord_cartesian(xlim = c(-3.5, 3.5), ylim = c(-2.5, 2.5))
+ggsave("figures/ordinationplot_latents.png", height = 5, width = 6)
 
 
-mod1$model_output |> View()
+ggplot(data = loadings_plot) +
+  geom_hline(yintercept = 0) +
+  geom_vline(xintercept = 0) +
+  geom_point(aes(x = LV1, y = LV2, fill = sd_rate), pch = 21, size = 4) +
+  ggpubr::theme_pubr()  +
+  scale_fill_distiller(palette = "Blues", direction = 1) +
+  labs(fill = "Average\ngrowth rate", x = "Latent variable 1", y = "Latent variable 2") +
+  theme(legend.position  = "right") +
+  coord_cartesian(xlim = c(-3.5, 3.5), ylim = c(-2.5, 2.5))
+ggsave("figures/ordinationplot_latents.png", height = 5, width = 6)
 
-draws_fit = mod1$model_output |> posterior::as_draws_matrix() # mvgam_draws() now exists too
-posterior_df = posterior::summarize_draws(draws_fit)
+plot(loadings_plot$sd_rate/loadings_plot$mean_rate ~ loadings_plot$LV2)
+m1 = lm(loadings_plot$sd_rate ~ loadings_plot$LV2)
+summary(m1)
+abline(a = 0.076, b = 0.01045)
+
+# try to correlate latents with fisheries and ????
+
+fishing_summary <- readr::read_csv("~/Documents/GitHub/groundfish-data-analysis/data/fishing_effort_data.csv")
+fishing_summary <- fishing_summary[which(fishing_summary$year > 1981) & which(fishing_summary$year < 2015),]
+fishing_summary <- dplyr::filter(fishing_summary, type == "benthic")
+fishing_effort_data = fishing_summary$catch[fishing_summary$type=="benthic"]/1e6
+fishing_summary$catch = fishing_effort_data
+
+lv = tidyr::separate(lv, col = variable, into = c("time", "LV"), sep = ",", remove = TRUE)
+lv$time = readr::parse_number(lv$time)
+lv$LV = readr::parse_number(lv$LV)
+
+A = ggplot(data = fishing_summary) +
+  geom_line(aes(x = year, y = catch)) +
+  labs(x = "Year", y = "Catch", title = "Potential drivers") +
+  ggpubr::theme_pubr()
 
 
-matplot(preds)
+B = ggplot(data = dplyr::filter(lv, LV == 1)) +
+  geom_ribbon(aes(x = time+1981, ymin = q5, ymax = q95), alpha = .3, fill = "navyblue") +
+  geom_line(aes(x = time+1981, y = median)) +
+  labs(x = "Year", y = "Latent variable 1", title = "Latent variation") +
+  ggpubr::theme_pubr()
 
-mvgam::plot_mvgam_trend(mod1, series = 1)
+A + B
 
-conditional_effects(mod1)
+climate_index <- readr::read_csv("~/Documents/GitHub/groundfish-data-analysis/data/composite_index.csv")
 
+C = ggplot(data = dplyr::filter(climate_index, year > 1981 & year <2015)) +
+  geom_line(aes(x = year, y = climate_index)) +
+  labs(x = "Year", y = "Composite climate index") +
+  ggpubr::theme_pubr()
 
-mod1$model_output |> dplyr::filter()
+D = ggplot(data = dplyr::filter(lv, LV == 2)) +
+  geom_ribbon(aes(x = time+1981, ymin = q5, ymax = q95), alpha = .3, fill = "navyblue") +
+  geom_line(aes(x = time+1981, y = median)) +
+  labs(x = "Year", y = "Latent variable 2") +
+  ggpubr::theme_pubr()
 
-# this is the "LPI", should take the derivative of just this. this is the 
-# common trajectory of the whole community.
-plot(mod1, type = "smooth")
-
-plot(mod1, type = "factors")
-
-plot(mod1, type = "residuals")
-
+(A + B) / (C + D) + plot_annotation(tag_levels = "a", tag_suffix = ")")
+ggsave("figures/latent_potentialdrivers.png", width = 7, height = 7)
